968152861829bb43d9b66a0fadbfbe38b53ebf07
Sean <seannmackay@gmail.com>
2020-09-22 23:18:34 -0400

    overhauled ui for home screen and steam menus

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-steamTuuls: A set of helpful tools for interacting with your games collection. Currently this program mostly interacts with GOG galaxy 2's database file. 
-     Without GOG Galaxy 2 installed, there is still some level of limited functionality in the form of GOG price information, library querying, and finding
-     out what steam games are drm free.
+steamTuuls: A set of helpful tools for quickly finding prices and seeing if
+            games are drm free.
 
 Author: Sean Mackay
 
@@ -9,19 +8,19 @@ Use:
                   Make sure urllib3 is installed by running
                     -"pip3 install urllib3"
 
-  -DO NOT CHANGE THE FOLDER STRUCTURE FOR THE APPLICATION FOLDER. 
+  -DO NOT CHANGE THE FOLDER STRUCTURE
 
-  -run the program with the command "python3 main.py " from the root
+  -run the program with the command "python3 main.py 'argv' " from the root
        directory of the application
-  -argv will be your use case argument. -NOTE: Argv is currently depricated from V0.0.2 onward in favour of command line arguments in the application.
+  -argv will be your use case argument.
 
-  ARGV: DEPRICATED      
+  ARGV:
     -'steamDB' : Generates a new database for the list of drm free steam games
     -'steamdrm?' : Allows you to check if a game has drm.
     -'gogDB' : Generates a new database file for all games on GOG
     -'onGog' :  Allows you to Query if games exist on GOG and whats on sale
 
-```
+
                          ______                     
  _________        .---"""      """---.              
 :______.-':      :  .--------------.  :             
@@ -40,7 +39,7 @@ Use:
                  .'.eeeeeeeeeeeeeeeeee.'.      :___:
       fsc      .'.eeeeeeeeeeeeeeeeeeeeee.'.         
               :____________________________:
-```
+
 
 DISCLAIMER: The goal is to promote games that do not use DRM utilities as they
             are detrimental to the customer experience. Illegal use of this
@@ -50,8 +49,6 @@ _______________________________________________________________________________
 Galaxy Integration:
 From their readme:
 Credit to original creators.
-  Their script can be found directly at: https://github.com/AB1908/GOG-Galaxy-Export-Script
-  This project uses a modified version of their original code. 
 
 NOTE: For galaxy integration to work, copy the file 'galaxy-2.0.db' from the
       install directory of GOG Galaxy 2.
--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import os
 import csv
 import sys
 import urllib, json
+import time
 
 import steam
 import gog
 import settings
-
-
+import help
 
 def createFile(cleanedList,fileName):
     with open(fileName, "w", newline="") as f:
@@ -29,44 +29,72 @@ def openFile(fileName):
 def main():
     settingStore=settings.loadSettings()
     settingStore2=settings.processSettings(settingStore)
-
+    os.system("clear")
     while 0==0:
+        os.system("clear")
+        print(help.pcol.Y+"WELCOME TO STEAMTUULS!"+help.pcol.ENDC)
         print(" ")
-        print("________________________________________________________________")
         print(" ")
-        print("Type 'steamDB' to regenerate the steam drm free games list.")
-        print("Type 'steamdrm?' to check and see if a game has drm.")
-        print("Type 'gogDB' to regenerate the GOG games database.")
-        print("Type 'onGog' to check discounts and see if GOG has a game.")
-        #print("Type 'onDisk' to generate lists of what games you have")
-        #print("^Note: This requires providing the directories via settings.txt")
-        print("Type 'galaxyDB' to generate the database for games in GOG Galaxy 2 DB")
-        print("Type 'galaxyquery' to see if you own the game and find out your stats.")
-        print("Type 'quit' to exit the tool.")
-        text=input("Response: ")
-        if text=="steamDB":
-            drmList=steam.drmListOpen()
-            print("parsed text file")
-            cleanedList=steam.listCleaner(drmList)
-            cleanedList=steam.listOrganize(cleanedList)
-            print("data cleaned")
-            createFile(cleanedList,"dataBases/SteamDRMFree.csv")
+        print(help.pcol.R+"                       ueeeeeu..")
+        print("  "+"                ur d$$$$$$$$$$$$$$Nu")
+        print("  "+"              d$$$ \"$$$$$$$$$$$$$$$$$$e.")
+        print("  "+"          u$$c   \"\"   ^\"^**$$$$$$$$$$$$$b.")
+        print("  "+"        z$$#\"\"\"           `!?$$$$$$$$$$$$$N.")
+        print("  "+"      .$P                   ~!R$$$$$$$$$$$$$b")
+        print("  "+"     x$F                 **$b. '\"R).$$$$$$$$$$")
+        print("  "+"    J^\"                           #$$$$$$$$$$$$.")
+        print("  "+"   z$e                      ..      \"**$$$$$$$$$")
+        print("  "+"  :$P           .        .$$$$$b.    ..  \"  #$$$$")
+        print("  "+"  $$            L          ^*$$$$b   \"      4$$$$L")
+        print("  "+" 4$$            ^u    .e$$$$e.\"*$$$N.       @$$$$$")
+        print("  "+" $$E            d$$$$$$$$$$$$$$L \"$$$$$  mu $$$$$$F")
+        print("  "+" $$&            $$$$$$$$$$$$$$$$N   \"#* * ?$$$$$$$N")
+        print("  "+" $$F            '$$$$$$$$$$$$$$$$$bec...z$ $$$$$$$$")
+        print("  "+"'$$F             `$$$$$$$$$$$$$$$$$$$$$$$$ '$$$$E\"$")
+        print("  "+" $$                  ^\"\"\"\"\"\"`       ^\"*$$$& 9$$$$N")
+        print("  "+" k  u$                                  \"$$. \"$$P r")
+        print("  "+" 4$$$$L                                   \"$. eeeR")
+        print("  "+"  $$$$$k                                   '$e. .@")
+        print("  "+"  3$$$$$b                                   '$$$$")
+        print("  "+"   $$$$$$                                    3$$\"")
+        print("  "+"    $$$$$  dc                                4$F")
+        print("  "+"     RF** <$$                                J\"")
+        print("  "+"      #bue$$$LJ$$$Nc.                        \"")
+        print("  "+"       ^$$$$$$$$$$$$$r")
+        print("  "+"         `\"*$$$$$$$$$")
+        print(help.pcol.ENDC)
+        print("_____________________________________________________________________________")
+        print(" ")
+        print(help.pcol.P + "Choose a command from the list below." + help.pcol.ENDC)
+        print(help.pcol.B + "sdb" + help.pcol.ENDC + " - generate steam drm free games database.")
+        print(help.pcol.B + "sdrm" + help.pcol.ENDC + " - check if a steam game has drm.")
+        print(help.pcol.B + "gdb" + help.pcol.ENDC + " - generate the GOG games database.")
+        print(help.pcol.B + "gog" + help.pcol.ENDC + " - interact with GOG store database.")
+        print(help.pcol.B + "galdb" + help.pcol.ENDC + " - generate database from your GOG Galaxy 2 library.")
+        print(help.pcol.B + "galax" + help.pcol.ENDC + " - interact with your GOG Galaxy 2 library.")
+        print(help.pcol.B + "quit" + help.pcol.ENDC + " - exit SteamTuuls.")
+        text=input(": ")
+        print("_____________________________________________________________________________")
+        print(" ")
+        if text=="sdb":
+            createFile(steam.drmListOpen(),"dataBases/SteamDRMFree.csv")
             print("database creation successful")
-        elif text=="steamdrm?":
+            time.sleep(3)
+        elif text=="sdrm":
             fetchedSteamDB=openFile("dataBases/SteamDRMFree.csv")
             steam.exists(fetchedSteamDB)
-        elif text =="gogDB":
+        elif text =="gdb":
             rawdb=gog.processGog()
             writeOut=gog.createRaw(rawdb)
             contents=gog.readRaw()
             cleanedFile=gog.cleaner(contents)
             createFile(cleanedFile,"dataBases/GOG.csv")
-        elif text=="onGog":
+        elif text=="gog":
             fetchedGogDB=openFile("dataBases/GOG.csv")
             gog.selectAction(fetchedGogDB)
-        elif text=="galaxyDB":
+        elif text=="galdb":
             settings.generateDB()
-        elif text == "galaxyquery":
+        elif text == "galaxy":
             opened_data=openFile("dataBases/gameDB.csv")
             settings.queryDB(opened_data)
         elif text=="quit":
@@ -74,6 +102,7 @@ def main():
             break
         else:
             print("Not a valid input please try again.")
+            time.sleep(3)
 
 
 
